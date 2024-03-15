d4c593510fe9fac17e1c7e072b9262fc99018b28
miladtaghizadeh1382 <miladtaghizadeh1382@outlook.com>
2024-03-16 00:46:25 +0330

    FEAT: auhorization guard for verifying accessToken is configured

---
+++ src/common/messages/auth.message.js
@@ -0,0 +1,11 @@
+const AuthorizationMessage = Object.freeze({
+    LoginSuccessfully: "login successfully !!",
+    Login: "login to your account first !!!!",
+    LoginFailed: "failed to login , try again !!!!",
+    LoginAgain: "please login again to your account !!!!",
+    Unauthorized: "UNAUTHORIZED, login again to your account",
+    NotFoundAccount: "account not found !!!!",
+    InvalidToken: "token is invalid !!"
+})
+
+module.exports = AuthorizationMessage;
