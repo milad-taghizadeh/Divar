52981360fcb9ea82b27f7dd3a5e695f7003f1726
miladtaghizadeh1382 <miladtaghizadeh1382@outlook.com>
2024-03-16 02:02:30 +0330

    FIX: authorization fixed , getting cookies and response of server for getting user info

--- src/common/guard/authorization.guard.js
+++ src/common/guard/authorization.guard.js
@@ -7,11 +7,11 @@ require('dotenv').config();
 
 const Authorization = async (req, res, next) => {
     try {
-        const token = req?.cookie?.accessToken;
+        const token = req?.cookies?.accessToken;
         if (!token) throw new createHttpError.Unauthorized(AuthorizationMessage.Login)
         const data = jwt.verify(token, process.env.JWT_SECRET_KEY)
         if (typeof data === 'object' && data?.id) {
-            const user = await UserModel.findById(data.id, {accessToken: 0, otp: 0}).lean();
+            const user = await UserModel.findById(data.id, { accessToken: 0, otp: 0, __v: 0, verifiedMobile: 0, updatedAt: 0 }).lean();
             if (!user) throw new createHttpError.NotFound(AuthorizationMessage.NotFoundAccount);
             req.user = user;
             return next();
